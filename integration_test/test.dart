020e9f660eea5d073ccade9ea8d4a53db22638e6
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-04-12 15:59:22 +0000

    Updating to latest FlutterFlow output.

--- integration_test/test.dart
+++ integration_test/test.dart
@@ -114,9 +114,6 @@ void main() async {
     await tester.enterText(
         find.byKey(const ValueKey('TextField_vp6n')), 'CSC305');
     await tester.pumpAndSettle(const Duration(milliseconds: 3000));
-    await tester.tap(find.byKey(const ValueKey('DropDown_6xvu')));
-    await tester.tap(find.text('High'));
-    await tester.pumpAndSettle(const Duration(milliseconds: 3000));
     await tester.enterText(
         find.byKey(const ValueKey('Container_xa4s')), '4/13/2026');
     await tester.pumpAndSettle(const Duration(milliseconds: 3000));
