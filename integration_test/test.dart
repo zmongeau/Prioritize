2137b7ea7b8595c81c147ed4a1f5ea59091f6688
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-04-14 03:53:26 +0000

    Updating to latest FlutterFlow output.

--- integration_test/test.dart
+++ integration_test/test.dart
@@ -143,6 +143,8 @@ void main() async {
         find.byKey(const ValueKey('passwordConfirm_fdsk')), 'password');
     await tester.pumpAndSettle(const Duration(milliseconds: 5000));
     await tester.tap(find.byKey(const ValueKey('SignUpButton_rorj')));
+    await tester.pump(kDoubleTapMinTime);
+    await tester.tap(find.byKey(const ValueKey('SignUpButton_rorj')));
     await tester.pumpAndSettle(const Duration(milliseconds: 10000));
     expect(find.text('Profile'), findsNothing);
   });
