3f0b1717e553cf582394f6b96d82c5ec5268ace0
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-05-04 19:00:05 +0000

    Updating to latest FlutterFlow output.

--- integration_test/test.dart
+++ integration_test/test.dart
@@ -46,8 +46,6 @@ void main() async {
     await tester.pumpAndSettle(const Duration(milliseconds: 3000));
     await tester.enterText(
         find.byKey(const ValueKey('password_g4ok')), 'password');
-    await tester.pumpAndSettle(const Duration(milliseconds: 5000));
-    await tester.tap(find.byKey(const ValueKey('SignUpPage_4agm')));
     await tester.pumpAndSettle(const Duration(milliseconds: 10000));
     await tester.tap(find.byKey(const ValueKey('LoginButton_hzgs')));
     await tester.pumpAndSettle(const Duration(milliseconds: 15000));
