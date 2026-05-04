89ad3c6c8584fcea13e3f16f888c962fd50e68e1
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-05-04 16:23:26 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/profile_page/profile_page_widget.dart
+++ lib/pages/profile_page/profile_page_widget.dart
@@ -86,6 +86,7 @@ class _ProfilePageWidgetState extends State<ProfilePageWidget> {
                               child: Align(
                                 alignment: AlignmentDirectional(0.0, -1.0),
                                 child: Text(
+                                  key: ValueKey('Text_yc5w'),
                                   'Profile',
                                   textAlign: TextAlign.center,
                                   style: FlutterFlowTheme.of(context)
