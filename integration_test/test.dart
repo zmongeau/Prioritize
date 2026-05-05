b19e7fba8274b322af7c3a2c1fe0e92fd1974ec0
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-05-05 17:00:49 +0000

    Updating to latest FlutterFlow output.

--- integration_test/test.dart
+++ integration_test/test.dart
@@ -68,6 +68,31 @@ void main() async {
     await tester.tap(find.byKey(const ValueKey('Button_j0ds')));
   });
 
+  testWidgets('SignOut', (WidgetTester tester) async {
+    _overrideOnError();
+
+    await tester.pumpWidget(ChangeNotifierProvider(
+      create: (context) => FFAppState(),
+      child: const MyApp(),
+    ));
+    await GoogleFonts.pendingFonts();
+
+    await tester.pumpAndSettle(const Duration(milliseconds: 5000));
+    await tester.enterText(find.byKey(const ValueKey('emailAddress_dacs')),
+        'robert.stevens@uri.edu');
+    await tester.pumpAndSettle(const Duration(milliseconds: 3000));
+    await tester.enterText(
+        find.byKey(const ValueKey('password_g4ok')), 'password');
+    await tester.pumpAndSettle(const Duration(milliseconds: 5000));
+    await tester.tap(find.byKey(const ValueKey('SignIn_tc28')));
+    await tester.pumpAndSettle(const Duration(milliseconds: 5000));
+    await tester.tap(find.byKey(const ValueKey('LoginButton_hzgs')));
+    await tester.pumpAndSettle(const Duration(milliseconds: 20000));
+    await tester.tap(find.byKey(const ValueKey('SignOutButton_koqx')));
+    await tester.pumpAndSettle(const Duration(milliseconds: 10000));
+    expect(find.byKey(const ValueKey('LoginButton_hzgs')), findsOneWidget);
+  });
+
   testWidgets('SignUp', (WidgetTester tester) async {
     _overrideOnError();
 
